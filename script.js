3bbc828ff3fb65a72083abe5ecd805018b64d3a8
Caleb <calebkiune@gmail.com>
2024-10-18 16:24:01 +0300

    Adds JS add function

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ document.addEventListener('DOMContentLoaded', ()=>{
   const input = document.getElementById("input");
   const button = document.getElementById("button");
   const taskList =document.getElementById('tasks');
-  
+
+  button.addEventListener('click', (e) => {
+    e.preventDefault()
+    const taskText = input.ariaValueMax.trim();
+    if (taskText !== '') {
+      const li =document.createElement('li');
+      li.classList.add('task');
+    }
+  })
 } )
