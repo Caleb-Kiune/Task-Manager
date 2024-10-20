805cc410f42939a36514e65a2626506396ac1037
Caleb <calebkiune@gmail.com>
2024-10-20 12:04:40 +0300

    Adds fetch

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-  const input = document.getElementById("input");
+  fetch('http://localhost:8000/tasks')
+  .then(response => response.json())
+  .then(tasks => {
+    const taskList = document.getElementById('tasks');
+    tasks.forEach(task => {
+      const li = document.createElement('li');
+      li.classList.add('task');
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      const labelText = document.createElement('label');
+      labelText.classList.add('text');
+      labelText.textContent = `${task.taskText} at ${task.taskTime} for ${task.taskDuration} mins`;
+      const editButton = document.createElement('button');
+      editButton.classList.add('edit');
+      editButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>';
+      const removeButton = document.createElement('button');
+      removeButton.classList.add('remove');
+      removeButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>';
+      li.appendChild(checkbox);
+      li.appendChild(labelText);
+      li.appendChild(editButton);
+      li.appendChild(removeButton);
+      taskList.appendChild(li);
+    });
+  })
+  .catch(error => console.error('Error:', error));
+});
+
+
+  /*const input = document.getElementById("input");
   const button = document.getElementById("button");
   const taskList =document.getElementById('tasks');
   const time = document.getElementById('time');
